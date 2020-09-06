62cdd34e80f9da0053db2fb4cc82d0d1008ca242
CharmaineChosen <charmaine.makete@gmail.com>
2020-09-07 01:53:02 +0200

    3 files modified

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component, VERSION } from '@angular/core';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'my-app',
@@ -6,5 +6,35 @@ import { Component, VERSION } from '@angular/core';
   styleUrls: [ './app.component.css' ]
 })
 export class AppComponent  {
-  name = 'Angular ' + VERSION.major;
+  title = 'Shopping List';
+  employees = [
+    {name: "Cheese", position:"four"},
+    {name: "Juice", position:"two"},
+    {name: "Apples", position:"thirty"},
+  ];
+  model:any={};
+  model2:any={};
+  addEmployee(){
+    this.employees.push(this.model)
+    this.model = {};
+  }
+  deleteEmployee(i){
+    this.employees.splice(i, 1)
+    console.log(i);
+  }
+  myValue;
+  editEmployee(k){
+    this.model2.name = this.employees[k].name;
+    this.model2.position = this.employees[k].position;
+    this.myValue = k;
+  }
+  updateEmployee(){
+    let k= this.myValue;
+    for(let i=0; i<this.employees.length;i++){
+      if(i==k){
+        this.employees[i]= this.model2;
+        this.model2 = {};
+      }
+    }
+  }
 }
